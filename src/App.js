4573a619e76e24fdaf1eb2d234f9ab70d444d507
Luca <luca.milanese90@gmail.com>
2021-04-14 17:16:00 +0200

    added basic App component css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import SearchContainer from './components/SearchContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <div className="auto-container">
+        <SearchContainer />
+      </div>
     </div>
   );
 }
