1d197cca3dffad552af929916ac4f4c90c23547a
Luca <luca.milanese90@gmail.com>
2021-04-28 16:12:31 +0200

    reverted dev

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-<<<<<<< HEAD
-import { useState, useReducer } from "react";
-import { Switch, Route, useHistory } from "react-router-dom";
-import { likeHandler } from "./services/utilityFunctions";
-import React from "react";
-import logo from "./assets/logoPlaceholder.jpg";
-import "./App.css";
-import ScrollToTop from "./services/ScrollToTop";
-import Home from "./components/home/Home";
-import CardPage from "./components/card-page/CardPage";
-import CardDetails from "./components/card-details/CardDetails";
-import CardList from "./components/card-list/CardList";
-=======
 import { useState, useReducer, useEffect } from 'react';
 import { Switch, Route, useHistory } from 'react-router-dom';
 import { likeHandler } from './services/utilityFunctions';
-import React from 'react';
 import logo from './assets/logoPlaceholder.jpg';
 import './App.css';
 import ScrollToTop from './services/ScrollToTop';
 import Home from './components/home/Home';
 import CardPage from './components/card-page/CardPage';
 import CardDetails from './components/card-details/CardDetails';
+import CardList from './components/card-list/CardList';
 import MediaContext from './context';
->>>>>>> card-details
 
 function App() {
   const history = useHistory();
@@ -32,8 +18,8 @@ function App() {
   const [mediaDetails, setMediaDetails] = useState();
   const [mediaCredits, setMediaCredits] = useState();
   const [dropdownSearchValue, setDropdownSearchValue] = useState({
-    title: "",
-    id: "",
+    title: '',
+    id: '',
   });
   const [swipedMedia, dispatchSwipedMedia] = useReducer(likeHandler, {
     liked: [],
@@ -47,53 +33,6 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
-      <ScrollToTop>
-        <header className="App-header">
-          <nav>
-            <img
-              id="logo"
-              alt="logo"
-              src={logo}
-              onClick={() => history.push("/")}
-            />
-          </nav>
-        </header>
-        <Switch>
-          <Route exact path="/">
-            <Home
-              setMediaList={setMediaList}
-              dropdownSearchValue={dropdownSearchValue}
-              setDropdownSearchValue={setDropdownSearchValue}
-            />
-          </Route>
-          <Route path="/card-page">
-            <CardPage
-              dropdownSearchValue={dropdownSearchValue}
-              mediaList={mediaList}
-              setMediaList={setMediaList}
-              displayIndex={displayIndex}
-              setDisplayIndex={setDisplayIndex}
-              setStreamingProvidersList={setStreamingProvidersList}
-              setMediaDetails={setMediaDetails}
-              setMediaCredits={setMediaCredits}
-              dispatchSwipedMedia={dispatchSwipedMedia}
-              swipedMedia={swipedMedia}
-            />
-          </Route>
-          <Route path="/card-details">
-            <CardDetails
-              streamingProvidersList={streamingProvidersList}
-              mediaDetails={mediaDetails}
-              mediaCredits={mediaCredits}
-            />
-          </Route>
-          <Route path="/card-list">
-            <CardList swipedMedia={swipedMedia} />
-          </Route>
-        </Switch>
-      </ScrollToTop>
-=======
       <MediaContext.Provider value={{ mediaType, setMediaType }}>
         <ScrollToTop>
           <header className="App-header">
@@ -125,6 +64,7 @@ function App() {
                 setMediaDetails={setMediaDetails}
                 setMediaCredits={setMediaCredits}
                 dispatchSwipedMedia={dispatchSwipedMedia}
+                swipedMedia={swipedMedia}
               />
             </Route>
             <Route path="/card-details">
@@ -134,10 +74,12 @@ function App() {
                 mediaCredits={mediaCredits}
               />
             </Route>
+            <Route path="/card-list">
+              <CardList swipedMedia={swipedMedia} />
+            </Route>
           </Switch>
         </ScrollToTop>
       </MediaContext.Provider>
->>>>>>> card-details
     </div>
   );
 }
